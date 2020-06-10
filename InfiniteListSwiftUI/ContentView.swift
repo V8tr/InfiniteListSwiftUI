e36263873254a00ca8273ddb807722fbdf63500d
V8tr <ne0_n@mail.ru>
2020-06-10 20:52:17 +0300

    Initial commit

---
+++ InfiniteListSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InfiniteListSwiftUI
+//
+//  Created by Vadim Bulavin on 6/10/20.
+//  Copyright © 2020 Vadim Bulavin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
